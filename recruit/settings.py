f32af30ae026192504490c6483829554fa347f3f
ericmuh <muhwezi1000@gmail.com>
2021-04-15 15:17:45 +0300

    update, remove unnessary files

--- recruit/settings.py
+++ recruit/settings.py
@@ -172,10 +172,23 @@ USE_TZ = True
 DEBUG = True
 
 # Static files (CSS, JavaScript, Images)
+
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = ''
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join('static'), )
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# STATIC_ROOT = ''
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = (os.path.join('static'), )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
